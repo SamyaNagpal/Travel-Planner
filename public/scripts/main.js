cc8fc876dbcab420239f0475a8d9147e4fcdaa25
Sneha Yadav <ydvsneha2203@gmail.com>
2025-04-09 13:44:24 +0530

    updated css

---
+++ public/scripts/main.js
@@ -0,0 +1,10 @@
+let nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () =>{
+    if(window.scrollY > 100){
+        nav.classList.add('sticky_nav');
+    }
+    else{
+        nav.classList.remove('sticky_nav');
+    }
+});
